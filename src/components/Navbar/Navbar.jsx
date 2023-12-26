b8d324a7508ec5089b2e07b44cc7ed606fe65313
Loyoenock <loyoenock@gmail.com>
2023-12-27 01:33:04 +0300

    Responsive menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Link, NavLink } from "react-router-dom";
 
@@ -7,6 +7,12 @@ import logo from "../../assets/logo.png";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => {
+    setClick(!click);
+  };
+
   return (
     <section className="header-section">
       <div className="container">
@@ -14,37 +20,40 @@ const Navbar = () => {
           <Link to="/" className="logo">
             <img src={logo} alt="logo" className="logo" width={100} />
           </Link>
-          <ul className="nav-item">
-            <li>
+          <ul className={click ? "nav-item active" : "nav-item"}>
+            <li onClick={handleClick}>
               <NavLink to="/" className="navigation-link">
                 Home
               </NavLink>
             </li>
-            <li>
+            <li onClick={handleClick}>
               <NavLink to="/about" className="navigation-link">
                 About
               </NavLink>
             </li>
-            <li>
+            <li onClick={handleClick}>
               <NavLink to="/ourcuisines" className="navigation-link">
                 Our Cuisines
               </NavLink>
             </li>
-            <li>
+            <li onClick={handleClick}>
               <NavLink to="/shipping" className="navigation-link">
                 Shipping
               </NavLink>
             </li>
-            <li>
+            <li onClick={handleClick}>
               <NavLink to="/Contact" className="navigation-link">
                 Contact
               </NavLink>
             </li>
             <button>Make an Order</button>
           </ul>
-          <div className="mobile-menu">
-            <FaBars size={22} style={{ color: "white" }} />
-            <FaTimes size={22} style={{ color: "white" }} />
+          <div className="mobile-menu" onClick={handleClick}>
+            {click ? (
+              <FaTimes size={22} style={{ color: "white" }} />
+            ) : (
+              <FaBars size={22} style={{ color: "white" }} />
+            )}
           </div>
         </div>
       </div>
