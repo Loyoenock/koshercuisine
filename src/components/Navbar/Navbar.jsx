997b2a92a035f8b2054d4b6e7b2a27d7759144fa
Loyoenock <loyoenock@gmail.com>
2023-12-26 23:34:11 +0300

    Menu items

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Link, NavLink } from "react-router-dom";
+
 import { FaBars, FaTimes } from "react-icons/fa";
 import logo from "../../assets/logo.png";
 const Navbar = () => {
@@ -7,7 +9,45 @@ const Navbar = () => {
     <section className="header-section">
       <div className="container">
         <div className="navbar">
-          <img src={logo} alt="logo" className="logo" width={100} />
+          <Link to="/" className="logo">
+            <img src={logo} alt="logo" className="logo" width={100} />
+          </Link>
+          <ul className="nav-item">
+            <li className="nav-link">
+              <NavLink to="/" className="nav-link">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-link">
+              <NavLink to="/about" className="nav-link">
+                About
+              </NavLink>
+            </li>
+            <li className="nav-link">
+              <NavLink to="/ourcuisines" className="nav-link">
+                OurCuisines
+              </NavLink>
+            </li>
+            <li className="nav-link">
+              <NavLink to="/shipping" className="nav-link">
+                {" "}
+                Shipping{" "}
+              </NavLink>
+            </li>
+            <li className="nav-link">
+              <NavLink to="/contact" className="nav-link">
+                {" "}
+                Contact
+              </NavLink>
+            </li>
+            <li>
+              <button>Make an Order</button>
+            </li>
+          </ul>
+          <div className="mobile-menu">
+            <FaBars size={22} style={{ color: "white" }} />
+            <FaTimes size={22} style={{ color: "white" }} />
+          </div>
         </div>
       </div>
     </section>
